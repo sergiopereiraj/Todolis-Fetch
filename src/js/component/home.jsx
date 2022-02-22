1761a6dce9e71b5f8ae38eb5ad36bef7722d2d92
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2022-02-22 18:16:56 +0000

    uno

---
+++ src/js/component/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Home = () => {
+	return (
+		<div className="text-center mt-5">
+			<h1>aqui estoy!!</h1>
+		</div>
+	);
+};
+
+export default Home;
